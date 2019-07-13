33f99cba7788e5c382b96411f4ce070ad13801da
Zoltan Puchovsky <zoltan.puchovsky@gmail.com>
2019-07-14 00:26:11 +0200

    Initial Commit

---
+++ PickerViewToLabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PickerViewToLabel
+//
+//  Created by Zoltan Puchovsky on 14/07/2019.
+//  Copyright © 2019 Zoltan Puchovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
